78058d1d3ca52b89d229460b463bb10246b2dc4c
Pulkit <pulkit.gera@research.iiit.ac.in>
2021-11-23 08:15:33 +0530

    added model and sampling

--- nerf_mlp.py
+++ nerf_mlp.py
@@ -4,26 +4,133 @@ from torch import nn
 
 class PosEmbedding(nn.Module):
 	def __init__(self,max_logscale,N_freqs,logscale=True):
-		super(PosEmbedding).__init__()
+		super(PosEmbedding,self).__init__()
 		self.functions = [torch.sin,torch.cos]
 
 		if logscale:
 			self.freqs = 2**torch.linspace(0,max_logscale,N_freqs)
 		else:
 			self.freqs = torch.linspace(1,2**max_logscale,N_freqs)
+		print(self.freqs)
 
 	def forward(self,x):
-		out = [x]
+		"""
+		Input is a 3x1 vector and output is 3x21 (if N_freq is 10)
+		Idea is that we project it to a higer dimension to be able to
+		encode higher frequencies.
+		[x] -> [x,sin(x),cos(x),sin(2x),cos(2x),......]
+		Inputs:
+		x : (Batch,3)
+		Outputs:
+		out : (Batch, 6*N_freqs+3)
+		"""
+		out = x
 		for freq in self.freqs:
 			for func in self.functions:
-				out += [func(freq*x)]
+				out = torch.cat((out,func(freq*x)),axis=1)
 
-		return torch.cat(out,-1)
+		return out
 
 
 class NeRF(nn.Module):
 
-	def __init__(self,t)
+	def __init__(self,types,density,width,skips,in_channels_xyz,in_channels_dir):
+		"""
+		types = "coarse", "fine"
+		density = no of layers for density (sigma) encoder
+		width = no of hidden units in each layer
+		skips = add skip connection in Dth layer
+		in_channels_xyz = no of input channels for xyz 3 + 6*10 = 63
+		in_channels_dir = no of input_channels for dir 3 + 6*4 = 27
+		"""
+		super(NeRF,self).__init__()
+		self.types = types
+		self.density = density
+		self.width = width
+		self.skips = skips
+		self.in_channels_xyz = in_channels_xyz
+		self.in_channels_dir = in_channels_dir
+
+		"""
+		The network architecture is 4-5 layers with input being Pos(x)
+		and at the 4th layer adding Pos(x) again. 2 layers later we add
+		encoded Pos(dir) and get the final color 
+		"""
+
+		# encode xyz
+		for i in range(self.density):
+			if i == 0:
+				layer = nn.Linear(in_channels_xyz,width)
+			elif i in skips:
+				layer = nn.Linear(width+in_channels_xyz,width)
+			else :
+				layer = nn.Linear(width,width)
+			layer = nn.Sequential(layer,nn.ReLU(True))
+			setattr(self,f'xyz_encoding_{i+1}',layer)
+		
+		self.xyz_encoding_final = nn.Linear(width,width)
+
+		#encode dir
+		self.dir_encoding = nn.Sequential(
+								nn.Linear(width+in_channels_dir,width//2),nn.ReLU(True))
+		self.sigma = nn.Sequential(nn.Linear(width,1),nn.Softplus())
+		self.rgb = nn.Sequential(nn.Linear(width//2,3),nn.Sigmoid())
+
+	def forward(self,x,sigma_only = False):
+		"""
+		Encodes x and dir
+		Input:
+			x : Pos(x,y,z) and Direction (The ray directions are the dir)
+			sigma_only : return only sigma
+		Outputs (concatenated):
+			rgb and sigma (Batch,4)
+		Render this ray in rendering.py
+		Sigma is the output of the second last layer while rgb is the final output
+		"""
+		if sigma_only:
+			input_xyz = x
+		else:
+			input_xyz, input_dir = torch.split(x,[self.in_channels_xyz,self.in_channels_dir],-1)
+
+		xyz_ = input_xyz
+
+		for i in range(self.density):
+			if i in self.skips:
+				xyz_ = torch.cat([input_xyz,xyz_],dim=-1)
+			xyz_ = getattr(self,f'xyz_encoding_{i+1}')(xyz_)
+
+		sigma = self.sigma(xyz_) # (B,1)
+		# print(sigma.shape)
+		if sigma_only:
+			return sigma
+		xyz_encoding_final = self.xyz_encoding_final(xyz_)
+
+		dir_encoding_input = torch.cat([xyz_encoding_final,input_dir],-1)
+		dir_encoding = self.dir_encoding(dir_encoding_input)
+
+		rgb = self.rgb(dir_encoding) #(B,3)
+		# print(rgb.shape)
+		output = torch.cat([rgb,sigma],-1) #(B,4)
+
+		return output
 
 if __name__ == '__main__':
-	
+	batch = 1024
+	xyz_L = 10
+	xyz_embedding = PosEmbedding(xyz_L-1,xyz_L)
+	input_x  = torch.rand(batch,3)
+	pos_x = xyz_embedding(input_x)
+	print(pos_x.shape)
+
+	dir_L = 4
+	dir_embedding = PosEmbedding(dir_L-1,dir_L)
+	input_dir  = torch.rand(batch,3)
+	pos_dir = dir_embedding(input_x)
+	print(pos_dir.shape)
+
+	nerf_input = torch.cat([pos_x,pos_dir],dim=1)
+	print(nerf_input.shape)
+	model = NeRF(types="test",density=8,width=256,skips=[4],in_channels_xyz=63,in_channels_dir=27)
+	nerf_output = model(nerf_input)
+	print(nerf_output.shape)
+
--- rendering.py
+++ rendering.py
@@ -4,8 +4,60 @@ from einops import rearrange, reduce, repeat
 __all__ = ['render_rays']
 
 
-def sample_pdf(bins,weights,N_importance,det=False, eps=1e-5):
-	pass
+def sample_pdf(bins, weights, N_importance, det=False, eps=1e-5):
+	"""
+	Importance sample the points on the ray
+	Distribution defined by weights
+	Inputs:
+		bins : (N_rays,N_samples_+1) N_samples_ = No of coarse samples per ray-2
+		weights : (N_rays, N_samples_ )
+		N_importance : No of samples to draw from the distribution
+		det : deterministic or not
+		eps: a small no
+	Outputs:
+		samples: the sampled samples
+	"""
+	NN_rays, N_samples_ = weights.shape
+	weights = weights + eps # prevent division by zero (don't do inplace op!)
+	pdf = weights / reduce(weights, 'n1 n2 -> n1 1', 'sum') # (N_rays, N_samples_)
+	cdf = torch.cumsum(pdf, -1) # (N_rays, N_samples), cumulative distribution function
+	cdf = torch.cat([torch.zeros_like(cdf[: ,:1]), cdf], -1)  # (N_rays, N_samples_+1) 
+                                                               # padded to 0~1 inclusive
+
+	if det:
+		u = torch.linspace(0, 1, N_importance, device=bins.device)
+		u = u.expand(N_rays, N_importance)
+	else:
+		u = torch.rand(N_rays, N_importance, device=bins.device)
+	u = u.contiguous()
+
+	inds = torch.searchsorted(cdf, u, right=True)
+	below = torch.clamp_min(inds-1, 0)
+	above = torch.clamp_max(inds, N_samples_)
+
+	inds_sampled = rearrange(torch.stack([below, above], -1), 'n1 n2 c -> n1 (n2 c)', c=2)
+	cdf_g = rearrange(torch.gather(cdf, 1, inds_sampled), 'n1 (n2 c) -> n1 n2 c', c=2)
+	bins_g = rearrange(torch.gather(bins, 1, inds_sampled), 'n1 (n2 c) -> n1 n2 c', c=2)
+
+	denom = cdf_g[...,1]-cdf_g[...,0]
+	denom[denom<eps] = 1 # denom equals 0 means a bin has weight 0, in which case it will not be sampled
+	                     # anyway, therefore any value for it is fine (set to 1 here)
+
+	samples = bins_g[...,0] + (u-cdf_g[...,0])/denom * (bins_g[...,1]-bins_g[...,0])
+	return samples
 
 def rendering():
 	pass
+
+if __name__ == '__main__':
+	N_rays = 3*3
+	N_samples = 3
+
+	bins =  torch.rand((N_rays,N_samples+1))
+	weights = torch.rand((N_rays,N_samples))
+	N_importance = 10
+	det = True 
+	eps = 1e-8
+
+	samples = sample_pdf(bins,weights,N_importance,det,eps)
+	print(samples)
